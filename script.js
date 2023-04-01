17f3e21311b25b219aa1c9cb6d87d2dfc7266a96
Caitlin Donahue <caitlindonahue@Caitlins-MacBook-Air.local>
2023-04-01 16:17:00 -0400

    added password length

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("hello");
+  var passwordLength = prompt("Choose a number between 8-128");
+  console.log(passwordLength);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
